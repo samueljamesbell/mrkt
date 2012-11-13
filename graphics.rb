fa805f174c0205ac4679e648ccad8f009decc0ab
Sam Bell <samueljamesbell@gmail.com>
2012-11-13 00:21:02 +0000

    Now using include_package

--- graphics.rb
+++ graphics.rb
@@ -1,13 +1,15 @@
 require 'java'
 
-java_import 'javax.swing.JFrame'
-java_import 'java.awt.Graphics'
+module GraphicsTest
+  include_package 'javax.swing'
+  include_package 'java.awt'
 
-frame = JFrame.new("ShapesDemo2D");
-frame.setDefaultCloseOperation(JFrame::EXIT_ON_CLOSE)
-#applet = ShapesDemo2D.new
-#frame.getContentPane.add "Center", applet
-#applet.init
-frame.pack
-#frame.setSize(Dimension.new(550,100));
-frame.setVisible(true);
+  frame = JFrame.new("ShapesDemo2D");
+  frame.setDefaultCloseOperation(JFrame::EXIT_ON_CLOSE)
+  #applet = ShapesDemo2D.new
+  #frame.getContentPane.add "Center", applet
+  #applet.init
+  frame.pack
+  #frame.setSize(Dimension.new(550,100));
+  frame.setVisible(true);
+end
