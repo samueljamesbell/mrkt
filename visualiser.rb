5458c66b730a5978de757f8f87c8b580358a2dc4
Sam Bell <samueljamesbell@gmail.com>
2012-11-14 12:04:16 +0000

    Increased renders-per-second; Offer rectangles cannot be smaller than their label; renamed ruby methods; bud fixes

--- visualiser.rb
+++ visualiser.rb
@@ -20,7 +20,7 @@ class Visualiser < javax.swing.JFrame
     @running = false
 
     setDefaultCloseOperation JFrame::EXIT_ON_CLOSE
-    setExtendedState Frame::MAXIMIZED_BOTH
+    setSize 550, Toolkit.getDefaultToolkit.getScreenSize.height
     setVisible true
 
     Thread.new { run }
@@ -31,7 +31,7 @@ class Visualiser < javax.swing.JFrame
 
     while @running
       repaint
-      sleep 0.5
+      sleep 0.2
     end
   end
 
@@ -42,12 +42,13 @@ class Visualiser < javax.swing.JFrame
   def paint g
     @g = g
     @dimensions = getSize
+    @font_metrics = @g.getFontMetrics
 
     @g.setRenderingHint RenderingHints::KEY_ANTIALIASING, RenderingHints::VALUE_ANTIALIAS_ON
     @g.clearRect 0, 0, @dimensions.width, @dimensions.height
 
-    drawAxes
-    drawLabels '<<< Bids  |  Asks >>>', 'Price'
+    draw_axes
+    draw_labels '<<< Bids  |  Asks >>>', 'Price'
 
     display_offers @exchange.bids.to_array.sort, @exchange.asks.to_array.sort
   end
@@ -59,8 +60,11 @@ class Visualiser < javax.swing.JFrame
     x = center - 10
     y = PADDING
 
+
     bids.each do |bid|
-      drawRectangle x - bid.quantity, y, bid.quantity, bid.price.to_s
+      label_width = @font_metrics.stringWidth bid.price.to_s
+      width = bid.remaining_quantity >= label_width + 4 ? bid.remaining_quantity : label_width + 4
+      draw_rectangle x - width, y, width, bid.price.to_s
       y += RECTANGLE_HEIGHT + 20
     end
 
@@ -68,28 +72,32 @@ class Visualiser < javax.swing.JFrame
     y = PADDING
 
     asks.each do |ask|
-      drawRectangle x, y, ask.quantity, ask.price.to_s
+      draw_rectangle x, y, ask.remaining_quantity, ask.price.to_s
       y += RECTANGLE_HEIGHT + 20
     end
   end
 
-  def drawAxes
+  def draw_axes
     @g.setStroke AXIS_STROKE
     @g.setPaint Color::lightGray
     @g.drawLine PADDING, PADDING, PADDING, @dimensions.height - PADDING
     @g.drawLine PADDING, @dimensions.height - PADDING, @dimensions.width - PADDING, @dimensions.height - PADDING
   end
 
-  def drawLabels x_label, y_label
-    fontMetrics = @g.getFontMetrics
-    x_label_width = fontMetrics.stringWidth x_label
+  def draw_labels x_label, y_label
+    x_label_width = @font_metrics.stringWidth x_label
 
     @g.setPaint Color::black
     @g.drawString x_label, (@dimensions.width / 2) - (x_label_width / 2), @dimensions.height - PADDING + 25
     @g.drawString y_label, PADDING - 35, @dimensions.height / 2
   end
 
-  def drawRectangle x, y, width, label
+  def draw_rectangle x, y, width, label
+    label_width = @font_metrics.stringWidth label
+    label_height = @font_metrics.getHeight
+
+    width = width >= label_width + 4 ? width : label_width + 4
+
     rectangle = RoundRectangle2D::Double.new x, y, width, RECTANGLE_HEIGHT, 5, 5
 
     @g.setStroke STROKE
@@ -98,10 +106,6 @@ class Visualiser < javax.swing.JFrame
 
     @g.setPaint GradientPaint.new x, y, MID_BLUE, x, y + RECTANGLE_HEIGHT, DARK_BLUE
     @g.fill rectangle
-
-    fontMetrics = @g.getFontMetrics
-    label_width = fontMetrics.stringWidth label
-    label_height = fontMetrics.getHeight
 
     @g.setPaint Color::white
     @g.drawString label, ((width - label_width) / 2) + x, ((RECTANGLE_HEIGHT - label_height) / 2) + y + label_height
