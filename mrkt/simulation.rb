55c1aee7d874f70ebef540c1b02a60c4823013f7
Sam Bell <samueljamesbell@gmail.com>
2013-02-05 11:40:39 +0000

    Simulation now calls Evolution

--- mrkt/simulation.rb
+++ mrkt/simulation.rb
@@ -1,5 +1,15 @@
 class Simulation
   def self.run config
     exchange = Exchange.new config
+    evolution = Evolution.new exchange
+    # zero = Zero.new exchange
+    # reinforcement = Reinforcement.new exchange
+
+    config['simulation_runs'].times do
+      exchange.run
+      evolution.optimise
+      # zero.optimise
+      # reinforcement.optimise
+    end
   end
 end
