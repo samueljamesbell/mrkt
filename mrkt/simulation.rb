c2651fc09930daaa4638544b30577ac62ccb3efa
Sam Bell <samueljamesbell@gmail.com>
2013-02-13 15:34:44 +0000

    Removed references to visualiser from simulator. Simulation no longer a singleton

--- mrkt/simulation.rb
+++ mrkt/simulation.rb
@@ -5,32 +5,21 @@ require_relative 'optimisers/evolution'
 require_relative 'optimisers/zero'
 
 class Simulation
-  include Singleton
-
-  def initialize
-    @visualiser = Visualiser.new
-  end
 
   def self.run
-    instance.run
-  end
-
-  def run
     evolution = Evolution.new
     zero = Zero.new
     # reinforcement = Reinforcement.new exchange
 
     CONFIG['simulation_runs'].times do |i| puts "Running simulation #{i}"
-      @visualiser.run
-
       Exchange.run
       evolution.optimise
       zero.optimise
       # reinforcement.optimise
       
       puts Exchange.traders.sort_by! {|t| t.performance}
-      @visualiser.stop
       Exchange.reset
     end
   end
+
 end
