83c8245ed2898bf49bf0770fade0f76658dbd7e8
Sam Bell <samueljamesbell@gmail.com>
2013-02-27 16:05:53 +0000

    Moved common functionality from genetic_optimiser

--- mrkt/optimisers/evolution.rb
+++ mrkt/optimisers/evolution.rb
@@ -1,20 +1,6 @@
 require 'mrkt/optimisers/genetic_optimiser'
 
 class Evolution < GeneticOptimiser 
-  def optimise
-    parents = best(50).combination(2).to_a.shuffle[0..(@population.size / 4)-1]
-    strategies = parents.map {|p| mutate(crossover(p[0].strategy, p[1].strategy))}
-    strategies.each {|s| spawn_trader s}
-    worst(25).each {|t| kill t}
-  end
-
-  def best percentage
-    @population.sort{|x, y| y.performance <=> x.performance}[0..(@population.size / (100 / percentage))-1]
-  end
-
-  def worst percentage
-    @population.sort{|x, y| x.performance <=> y.performance}[0..(@population.size / (100 / percentage))-1]
-  end
 
   def crossover mother, father
     child = {}
@@ -35,13 +21,4 @@ class Evolution < GeneticOptimiser
     return strategy
   end
 
-  def spawn_trader strategy
-    trader = Trader.new strategy, self.class
-    @population << trader
-  end
-
-  def kill trader
-    Exchange.deregister trader
-    @population.delete trader
-  end
 end
--- mrkt/optimisers/genetic_optimiser.rb
+++ mrkt/optimisers/genetic_optimiser.rb
@@ -2,4 +2,30 @@ require 'mrkt/optimiser'
 
 class GeneticOptimiser < Optimiser
   abstract
+
+  def optimise
+    parents = best(50).combination(2).to_a.shuffle[0..(@population.size / 4)-1]
+    strategies = parents.map {|p| mutate(crossover(p[0].strategy, p[1].strategy))}
+    strategies.each {|s| spawn_trader s}
+    worst(25).each {|t| kill t}
+  end
+
+  def best percentage
+    @population.sort{|x, y| y.performance <=> x.performance}[0..(@population.size / (100 / percentage))-1]
+  end
+
+  def worst percentage
+    @population.sort{|x, y| x.performance <=> y.performance}[0..(@population.size / (100 / percentage))-1]
+  end
+
+  def spawn_trader strategy
+    trader = Trader.new strategy, self.class
+    @population << trader
+  end
+
+  def kill trader
+    Exchange.deregister trader
+    @population.delete trader
+  end
+
 end
