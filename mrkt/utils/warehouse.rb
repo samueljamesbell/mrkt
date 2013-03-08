4a1efd3e900b43327dfea513c3d9e8ef37b0b226
Sam Bell <samueljamesbell@gmail.com>
2013-03-08 14:57:01 +0000

    Refactored Warehouse

--- mrkt/utils/warehouse.rb
+++ mrkt/utils/warehouse.rb
@@ -9,7 +9,8 @@ class Warehouse
 
   def initialize
     @graphite = Graphite.new :host => '127.0.0.1', :port => 2003
-    @prices = []
+
+    @transaction_prices = []
     @transaction_times = []
     @dividends = []
 
@@ -19,23 +20,33 @@ class Warehouse
   end
 
   def log_transaction price
-    @prices << price
-    t = Time.now
-    @transaction_times << t
+    time = Time.now
+    @transaction_prices << price
+    @transaction_times << time
 
-    @graphite.push_to_graphite do |graphite|
-      graphite.puts "mrkt.transactions.prices #{price} #{@graphite.time_now}"
-    end
+    graphite_log "mrkt.transactions.prices", price
 
-    Thread.new { calculate_price_regression t }
+    # Perform other calculations here
+    Thread.new { calculate_price_regression time }
     Thread.new { calculate_dividend_regression }
     Thread.new { calculate_average_dividend }
-    #perform other calculations here
+  end
+
+  def log_dividend amount
+    @dividends << amount
+  end
+
+  def log_trader_performance trader
+    graphite_log "mrkt.traders.#{trader.optimiser.to_s.snake_case}.#{trader.object_id}.performance", trader.performance
+  end
+
+  def log_optimiser_performance optimiser
+    graphite_log "mrkt.optimisers.#{optimiser.class.to_s.snake_case}.performance", optimiser.performance
   end
 
   def transactions
     result = {}
-    @prices.size.times {|i| result[zeroed_transaction_times[i]] = @prices[i]}
+    @transaction_prices.size.times {|i| result[zeroed_transaction_times[i]] = @transaction_prices[i]}
 
     result
   end
@@ -45,42 +56,12 @@ class Warehouse
     @transaction_times.map {|t| t.to_i - start_time}
   end
 
-  def transactions_for_chart
-    [zeroed_transaction_times, @prices]
-  end
-
-  def transactions_to_csv
-    CSV.open("data/#{Time.now}.csv", "w") do |csv|
-      transactions.each {|t| csv << t}
-    end
-  end
-
-  def log_dividend amount
-    @dividends << amount
-  end
-
   def dividends
     @dividends
   end
 
-  def dividends_for_chart
-    @dividends
-  end
-
-  def log_trader_performance trader
-    @graphite.push_to_graphite do |graphite|
-      graphite.puts "mrkt.traders.#{trader.optimiser.to_s.snake_case}.#{trader.object_id}.performance #{trader.performance} #{@graphite.time_now}"
-    end
-  end
-
-  def log_optimiser_performance optimiser
-    @graphite.push_to_graphite do |graphite|
-      graphite.puts "mrkt.optimisers.#{optimiser.class.to_s.snake_case}.performance #{optimiser.performance} #{@graphite.time_now}"
-    end
-  end
-
   def calculate_price_regression time
-    prices = @transactions
+    prices = transactions
     if !prices.empty?
       x_vector = prices.keys.to_vector(:scale)
       y_vector = prices.values.to_vector(:scale)
@@ -90,19 +71,24 @@ class Warehouse
   end
 
   def calculate_dividend_regression
-    dividends = @dividends
-    if !dividends.empty?
-      x_vector = dividends.to_vector(:scale)
-      y_vector = (0..dividends.size-1).to_vector(:scale)
+    if !@dividends.empty?
+      x_vector = @dividends.to_vector(:scale)
+      y_vector = (0..@dividends.size-1).to_vector(:scale)
       regression = Statsample::Regression.simple(x_vector, y_vector)
-      @dividend_regression = regression.y(dividends.size)
+      @dividend_regression = regression.y(@dividends.size)
     end
   end
 
   def calculate_average_dividend
     @average_dividend = @dividends.inject(:+) / @dividends.size unless @dividends.empty?
   end
-  
+
+  private
+
+  def graphite_log key, value
+    @graphite.push_to_graphite {|g| g.puts "#{key} #{value} #{@graphite.time_now}"}
+  end
+
   def_delegators :instance, *Warehouse.instance_methods(false)
 
 end
